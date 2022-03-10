7e740c74c5c2cec9e4bedb3d006f7e603441c526
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2022-03-10 11:01:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from psd_tools import PSDImage
 import os
 
 files = os.listdir('./')
-files.remove('main.py')
-print(files)
-for file_name in files:
-	print('converting ',file_name)
-	psd = PSDImage.open(file_name)
-	psd.composite().save(f"{file_name.replace('.psd','')}.pdf")
+for index, file_name in enumerate(files):
+	if '.psd' in file_name:
+		print(f'converting {index}',file_name)
+		psd = PSDImage.open(file_name)
+		psd.composite().save(f"{file_name.replace('.psd','')}.pdf")
