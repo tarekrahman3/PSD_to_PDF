857b06d45867ffc443756131e6147244f953111f
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2022-04-01 05:22:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from psd_tools import PSDImage
-import os
+import os, tkinter, psd_tools
 
-for index, file_name in enumerate(os.listdir('./')):
+root = tkinter.Tk()
+root.withdraw()
+
+file_path = tkinter.filedialog.askdirectory()
+
+for index, file_name in enumerate(os.listdir(file_path)):
 	if '.psd' in file_name:
 		print(f'converting {index} ',file_name)
-		PSDImage.open(file_name).composite().save(f"{file_name.replace('.psd','')}.pdf")
+		psd_tools.PSDImage
+			.open(file_name)
+			.composite()
+			.save(f"{file_name.replace('.psd','')}.pdf")
